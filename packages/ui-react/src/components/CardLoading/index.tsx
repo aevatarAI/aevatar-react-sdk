3f199117abef3b8f21e271f978d6d3e7a3e3c5ee
potter <potter.sun@aelf.io>
2025-05-27 15:36:18 +0800

    feat: 🎸 add card loading

--- packages/ui-react/src/components/CardLoading/index.tsx
+++ packages/ui-react/src/components/CardLoading/index.tsx
@@ -1,11 +1,12 @@
+import clsx from "clsx";
 import Loading from "../../assets/svg/loading.svg?react";
 import "./index.css";
 
-export default function CardLoading() {
+export default function CardLoading({ className }: { className?: string }) {
   return (
     <div
       data-testid="card-loading"
-      className="sdk:flex sdk:justify-center sdk:items-center sdk:h-full">
+      className={clsx("sdk:flex sdk:justify-center sdk:items-center sdk:h-full", className)}>
       <Loading className="aevatarai-loading-icon" />
     </div>
   );
