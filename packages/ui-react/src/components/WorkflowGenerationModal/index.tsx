a5eeffe923c92efe3325edd0cb340d8c95f3ef94
cjhroy <roy.cheong@aelf.io>
2025-07-31 17:33:02 +0800

    added dialog

--- packages/ui-react/src/components/WorkflowGenerationModal/index.tsx
+++ packages/ui-react/src/components/WorkflowGenerationModal/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Textarea } from "../ui/textarea";
-import { Button } from "../ui";
+import { Button, Dialog, DialogContent, DialogTrigger } from "../ui";
 import Loading from "../../assets/svg/loading.svg?react";
 import AIStar from "../../assets/svg/aiStar.svg?react";
 import Close from "../../assets/svg/close.svg?react";
@@ -42,9 +42,14 @@ export const WorkflowGenerationModal = ({
   }
 
   return (
-    <>
-      <div className="sdk:bg-[#000000] sdk:fixed sdk:inset-0 sdk:opacity-50 sdk:z-99" />
-      <div className="sdk:flex sdk:justify-center sdk:p-[20px] sdk:bg-[#171717] sdk:absolute sdk:top-[25%] sdk:border sdk:border-[#FFFFFF14] sdk:rounded-md sdk:z-100">
+    <Dialog open={true} onOpenChange={handleClose}>
+      {/* <DialogTrigger asChild /> */}
+      {/* <div className="sdk:bg-[#000000] sdk:fixed sdk:inset-0 sdk:opacity-50 sdk:z-99" /> */}
+      <DialogContent
+        onClick={(e) => e.stopPropagation()}
+        aria-describedby="ai generate workflow"
+        className="sdk:bg-[#171717] sdk:min-w-[635px] sdk:p-[20px] sdk:flex sdk:flex-col sdk:rounded-md sdk:border sdk:border-[#303030]"
+      >
         <div className="sdk:flex sdk:flex-col sdk:gap-[28px]">
           <div className="sdk:flex sdk:justify-between">
             <span className="sdk:font-outfit sdk:font-semibold sdk:text-[18px] sdk:bg-gradient-to-r sdk:from-white sdk:to-[#999] sdk:bg-clip-text sdk:text-transparent sdk:lowercase">
@@ -60,9 +65,9 @@ export const WorkflowGenerationModal = ({
           </div>
 
           <div className="sdk:flex sdk:flex-col sdk:gap-2">
-            <span className="sdk:text-[14px] sdk:text-[#B9B9B9]-600">
+            <div className="sdk:font-outfit sdk:font-semibold sdk:text-[14px] sdk:text-[#B9B9B9]">
               prompt
-            </span>
+            </div>
             <Textarea
               className="sdk:text-[13px] sdk:bg-[#171717] sdk:min-w-[595px] sdk:min-h-[120px]"
               placeholder="please describe what kind of agent workflow you want to create"
@@ -72,15 +77,10 @@ export const WorkflowGenerationModal = ({
           </div>
 
           <div className="sdk:flex sdk:flex-row sdk:justify-between">
-            <Button
-              type="button"
-              className="max-w-[35px] max-h-[35px]"
-              onClick={handleClose}
-              disabled={isLoading}
-            >
-              <span className="sdk:text-[14px] sdk:text-[#B9B9B9]-600">
+            <Button type="button" onClick={handleClose} disabled={isLoading}>
+              <div className="sdk:font-semibold sdk:text-[12px] sdk:text-white hover:!sdk:text-[#303030] cursor-pointer">
                 skip
-              </span>
+              </div>
             </Button>
             <Button
               type="button"
@@ -97,15 +97,61 @@ export const WorkflowGenerationModal = ({
               ) : (
                 <div className="sdk:flex sdk:flex-row sdk:items-center sdk:gap-[5px]">
                   <AIStar />
-                  <span className="sdk:text-[14px] sdk:text-[#B9B9B9]-600">
+                  <div className="sdk:font-semibold sdk:text-[12px] sdk:text-white !hover:sdk:text-[#303030] cursor-pointer">
                     generate
-                  </span>
+                  </div>
                 </div>
               )}
             </Button>
           </div>
         </div>
-      </div>
-    </>
+      </DialogContent>
+    </Dialog>
   );
 };
+
+// <Dialog open={Boolean(saveFailed)} onOpenChange={onOpenChange}>
+// <DialogTrigger asChild />
+// <DialogContent
+//   onClick={(e) => e.stopPropagation()}
+//   aria-describedby="delete g-agent"
+//   className="sdk:w-[328px] sdk:p-[20px] sdk:flex sdk:flex-col sdk:rounded-[6px] sdk:border sdk:border-[#303030]">
+//   <div className="sdk:flex sdk:items-center sdk:justify-between">
+//     <div />
+//     <Close onClick={() => onOpenChange?.(false)} />
+//   </div>
+//   <div className="sdk:flex sdk:flex-col sdk:items-center sdk:gap-[16px] sdk:pt-[10px]">
+//     <SaveFailedTip />
+
+//     <div className="sdk:text-center sdk:font-outfit sdk:text-[18px] sdk:w-[274px] sdk:font-semibold sdk:leading-normal sdk:lowercase sdk:text-white">
+//       {saveFailed === SaveFailedError.insufficientQuota && (
+//         <>
+//           saving failed :( <br />
+//           Please purchase more quota
+//         </>
+//       )}
+//       {saveFailed === SaveFailedError.maxAgents &&
+//         "Maximum agent creation count reached"}
+//       {saveFailed === SaveFailedError.workflowExecutionFailed &&
+//         "Workflow execution failed"}
+//     </div>
+//     <div className="sdk:w-[220px] sdk:text-[12px] sdk:text-[#B9B9B9] sdk:text-center sdk:font-outfit sdk:font-normal sdk:leading-normal sdk:lowercase">
+//       {saveFailed === SaveFailedError.workflowExecutionFailed &&
+//         "The workflow encountered an error and could not complete. Please review the configuration and logs, then try again."}
+//     </div>
+//   </div>
+//   <div className="sdk:flex sdk:justify-between sdk:items-start sdk:self-stretch  sdk:gap-[14px] sdk:pt-[28px]">
+//     <Button
+//       className="sdk:text-[12px] sdk:bg-white sdk:text-[#303030] sdk:py-[7px] sdk:leading-[14px] sdk:font-semibold sdk:flex-1 sdk:lowercase"
+//       onClick={(e) => {
+//         e.stopPropagation();
+//         onSaveFailed?.(saveFailed);
+//         onOpenChange?.(false);
+//       }}>
+//       {saveFailed === SaveFailedError.insufficientQuota && "Purchase now"}
+//       {saveFailed === SaveFailedError.maxAgents && "ok"}
+//       {saveFailed === SaveFailedError.workflowExecutionFailed && "ok"}
+//     </Button>
+//   </div>
+// </DialogContent>
+// </Dialog>
