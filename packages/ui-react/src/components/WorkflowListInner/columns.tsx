89e331958befd68a94bf1faeedb8cd766446f171
potter <potter.sun@aelf.io>
2025-09-17 14:01:26 +0800

    refactor: 💡 change api field

--- packages/ui-react/src/components/WorkflowListInner/columns.tsx
+++ packages/ui-react/src/components/WorkflowListInner/columns.tsx
@@ -32,8 +32,8 @@ export const workflowColumns: ColumnDef<
     header: "created",
     cell: ({ row }) => (
       <div className="sdk:text-[12px]  sdk:font-outfit sdk:font-semibold">
-        {row.original.ctime
-          ? dayjs.utc(row.original.ctime).local().format("YYYY-MM-DD HH:mm")
+        {row.original.createTime
+          ? dayjs.utc(row.original.createTime).local().format("YYYY-MM-DD HH:mm")
           : "-"}
       </div>
     ),
